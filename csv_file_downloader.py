f8d9351559512cc24c65dbc7f7dd3ae53e927a72
BASAVARAJ BIJALI <67173421+Basavaraj100@users.noreply.github.com>
2021-09-17 10:46:06 +0530

    Add files via upload

---
+++ csv_file_downloader.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 15 11:00:19 2021
+
+@author: HP
+"""
+import streamlit as st 
+import streamlit.components as stc
+
+# Utils
+import base64 
+import time
+timestr = time.strftime("%Y%m%d-%H%M%S")
+import pandas as pd 
+
+
+
+def csv_downloader(data):
+	csvfile = data.to_csv()
+	b64 = base64.b64encode(csvfile.encode()).decode()
+	new_filename = "new_text_file_{}_.csv".format(timestr)
+	st.markdown("#### Download File ###")
+	href = f'<a href="data:file/csv;base64,{b64}" download="{new_filename}">Click Here!!</a>'
+	st.markdown(href,unsafe_allow_html=True)
+    
+    
+    
+    
+    
+
+if __name__=='__main__':
+    csv_downloader()
